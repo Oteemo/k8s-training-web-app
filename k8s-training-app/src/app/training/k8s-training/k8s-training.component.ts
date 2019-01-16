b88c957636375369adc299d1f792a1f72dc5c672
Talha Siddique <tsiddique@oteemo.com>
2019-01-16 16:07:12 -0500

    added environment variables, baseline functionality for uploading images and encoding to base64 string

--- k8s-training-app/src/app/training/k8s-training/k8s-training.component.ts
+++ k8s-training-app/src/app/training/k8s-training/k8s-training.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+
 @Component({
   selector: 'app-k8s-training',
   templateUrl: './k8s-training.component.html',
   styleUrls: ['./k8s-training.component.css']
 })
-export class K8sTrainingComponent implements OnInit {
+export class K8sTrainingComponent {
+   image: File;
+   baseAddress: string;
+   base64TextString: any;
 
-  constructor() { }
+   constructor() {
 
-  ngOnInit() {
   }
+
+  onImageChanged(event) {
+    this.image= event.target.files[0];
+    var reader = new FileReader();
+    reader.onload =this._handleReaderLoaded.bind(this);
+    reader.readAsBinaryString(this.image);
+  }
+
+   onUpload( ) {
+    this.baseAddress = environment.apiUrl + '/fileUpload';
+    //alert(this.base64TextString);
+
+  //   this.http.post('my-backend.com/file-upload', this.image)
+  //   .subscribe(...);
+  }
+ 
+  _handleReaderLoaded(readerEvt) {
+    var binaryString = readerEvt.target.result;
+    this.base64TextString= btoa(binaryString);
+    
+   }
+  
+  
 
 }

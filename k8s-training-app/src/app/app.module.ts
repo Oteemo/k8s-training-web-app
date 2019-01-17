e02f5eff0d1c22f86dba61dd0e3ffbf6cde9adde
Talha Siddique <tsiddique@oteemo.com>
2019-01-17 16:04:48 -0500

    implemented image upload, encoding and decoding, as well as display of downloaded image

--- k8s-training-app/src/app/app.module.ts
+++ k8s-training-app/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { K8sTrainingComponent } from './training/k8s-training/k8s-training.component';
 
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
     K8sTrainingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- k8s-training-app/src/app/training/k8s-training/k8s-training.component.ts
+++ k8s-training-app/src/app/training/k8s-training/k8s-training.component.ts
@@ -1,43 +1,68 @@
 import { Component, OnInit } from '@angular/core';
-
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpResponse, } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 
-@Component({
+interface DataResponse {
+  image_name: string;
+  processed_image: string;
+}
+@Component({ 
   selector: 'app-k8s-training',
   templateUrl: './k8s-training.component.html',
   styleUrls: ['./k8s-training.component.css']
 })
-export class K8sTrainingComponent {
-   image: File;
+export class K8sTrainingComponent implements OnInit{
+   image: any;
+   imageName: string;
    baseAddress: string;
+   restApi: string;
    base64TextString: any;
-
-   constructor() {
+   endpoint: string;
+   location: string;
+   headers: HttpHeaders;
+   constructor(private http:HttpClient) {
 
   }
+  ngOnInit() {
+    this.location = environment.location;
+    this.baseAddress =  environment.apiUrl;
+    this.restApi = '/v0.1/pedestrian_detector';
+    this.endpoint = this.baseAddress + this.restApi;
+    this.image = '';
+  }
 
+  //reads image, makes call to convert it to base 64 string
   onImageChanged(event) {
     this.image= event.target.files[0];
+    this.imageName = this.image.name;
     var reader = new FileReader();
     reader.onload =this._handleReaderLoaded.bind(this);
     reader.readAsBinaryString(this.image);
   }
 
-   onUpload( ) {
-    this.baseAddress = environment.apiUrl + '/fileUpload';
-    //alert(this.base64TextString);
+   //performs post request to endpoint
+   uploadImage( ) {
+    let data = {
+      'encoded_image': this.base64TextString,
+      'image_name':this.imageName
+    }
+      
+    this.http.post<DataResponse>(this.endpoint, data ).subscribe( 
+      response => { 
+      alert('Success!');
+      this.image = "data:image/jpeg;base64,"+response.processed_image;
 
-  //   this.http.post('my-backend.com/file-upload', this.image)
-  //   .subscribe(...);
+    }, 
+      (err: HttpErrorResponse) => {
+        alert(err.message);
+      },    
+    );
   }
- 
+   //converts image to base64 string
   _handleReaderLoaded(readerEvt) {
     var binaryString = readerEvt.target.result;
-    this.base64TextString= btoa(binaryString);
-    
-   }
-  
-  
+    this.base64TextString= btoa(binaryString); 
+    this.image = "data:image/jpeg;base64,"+this.base64TextString;
+  }
 
 }
